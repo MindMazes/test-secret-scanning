693eebd8c849f94c55e4b3253b0e4f8e2097596c
MindMazes <vargas@christiancav.com>
2025-03-27 10:48:36 -0700

    Create tdx-get-token.py

---
+++ tdx-get-token.py
@@ -0,0 +1,12 @@
+// Get TDX Token
+customer = "test"
+url = f"https://{customer}.teamdynamix.com/TDWebApi/api/auth/login"
+data = {
+  "username": "christian",
+  "password": "password123"
+}
+headers = {
+  'Content-Type': 'application/json'
+}
+response = requests.post(url, json=data, headers=headers)
+print(response.text)
