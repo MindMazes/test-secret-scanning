a68ee41dd88a4113dc345519310b41033bea68c8
MindMazes <vargas@christiancav.com>
2025-03-27 11:04:24 -0700

    Update tdx-get-token.py

--- tdx-get-token.py
+++ tdx-get-token.py
@@ -1,12 +1,15 @@
 // Get TDX Token
-customer = "test"
-url = f"https://{customer}.teamdynamix.com/TDWebApi/api/auth/login"
-data = {
-  aws_access_key_id: "christian",
-  aws_secret_access_key: "password123"
-}
-headers = {
-  'Content-Type': 'application/json'
-}
-response = requests.post(url, json=data, headers=headers)
-print(response.text)
+def test_api():
+  customer = "test"
+  url = f"https://{customer}.teamdynamix.com/TDWebApi/api/auth/login"
+  data = {
+    "username": "christian",
+    "password": "password123"
+  }
+  headers = {
+    'Content-Type': 'application/json'
+  }
+  response = requests.post(url, json=data, headers=headers)
+  print(response.text)
+
+test_api()
